43899f5b2a543cc1aac2f29a11cdedc40e69a085
Alin Babu <alinbabu2010@gmail.com>
2022-08-26 13:41:03 +0530

    Add common composable for progress bar and error text

---
+++ app/src/main/java/com/sample/mealzapp/ui/components/ViewComponents.kt
@@ -0,0 +1,42 @@
+package com.sample.mealzapp.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.style.TextAlign
+import com.sample.mealzapp.R
+
+@Composable
+fun ProgressErrorView(
+    isProgress: Boolean = false,
+    message: String = ""
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+
+        if (isProgress) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(dimensionResource(R.dimen.progress_bar_size))
+            )
+        } else {
+            Text(
+                text = message,
+                modifier = Modifier.padding(dimensionResource(R.dimen.error_text_padding)),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.body2
+            )
+
+        }
+
+    }
+}
