023d345b576783089eddf69a3a8b54415b3501f3
Alin Babu <alinbabu2010@gmail.com>
2022-08-26 16:44:19 +0530

    Modify repository class
    Add function to get meal category and varaiable to store meal categories list

--- app/src/main/java/com/sample/mealzapp/data/repository/MealsRepository.kt
+++ app/src/main/java/com/sample/mealzapp/data/repository/MealsRepository.kt
@@ -1,12 +1,31 @@
 package com.sample.mealzapp.data.repository
 
+import com.sample.mealzapp.data.models.Category
 import com.sample.mealzapp.data.network.MealsNetworkDataSource
+import com.sample.mealzapp.data.wrappers.ResponseWrapper
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class MealsRepository @Inject constructor(
     private val mealsNetworkDataSource: MealsNetworkDataSource
 ) {
 
-    suspend fun getMealsCategories() = mealsNetworkDataSource.getMealsCategories()
+    private var cachedMeals = listOf<Category>()
+
+    suspend fun getMealsCategories(): ResponseWrapper<List<Category>> {
+        val response = mealsNetworkDataSource.getMealsCategories()
+        cachedMeals = when (response) {
+            is ResponseWrapper.Error -> cachedMeals
+            is ResponseWrapper.Success -> response.data ?: cachedMeals
+        }
+        return response
+    }
+
+    fun getMeal(id: String?): Category? {
+        return cachedMeals.firstOrNull {
+            it.id == id
+        }
+    }
 
 }
