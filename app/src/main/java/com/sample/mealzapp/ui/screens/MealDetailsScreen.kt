b7d3d08c846ca8453a9aeafee962f15ee56ddeb3
Alin Babu <alinbabu2010@gmail.com>
2022-08-26 17:07:28 +0530

    Add image animation and modify alignment
    Also modify and add necessary dimension resources

--- app/src/main/java/com/sample/mealzapp/ui/screens/MealDetailsScreen.kt
+++ app/src/main/java/com/sample/mealzapp/ui/screens/MealDetailsScreen.kt
@@ -1,37 +1,54 @@
 package com.sample.mealzapp.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.Dp
 import coil.compose.AsyncImage
 import com.sample.mealzapp.R
 import com.sample.mealzapp.data.models.Category
 
 @Composable
 fun MealDetailsScreen(category: Category) {
-    Column {
-        Row {
+
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+
+    val imageSize: Dp by animateDpAsState(
+        targetValue = if (isExpanded)
+            dimensionResource(R.dimen.meal_image_size_expanded)
+        else dimensionResource(R.dimen.meal_image_size_collapsed)
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
             Card {
                 AsyncImage(
                     model = category.imageUrl,
                     contentDescription = "Meal image",
                     modifier = Modifier
                         .clip(CircleShape)
-                        .size(dimensionResource(R.dimen.meal_image_size))
+                        .size(imageSize)
+                        .padding(dimensionResource(R.dimen.meal_image_padding))
                 )
             }
             Text(text = category.name)
         }
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = { isExpanded = !isExpanded }) {
             Text(text = "Change state of meal profile picture")
         }
     }
