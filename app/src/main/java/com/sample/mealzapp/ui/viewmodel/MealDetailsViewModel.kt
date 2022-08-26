bf6ac2d18a31bd97b0cd9bc8d16f7ec15a77a513
Alin Babu <alinbabu2010@gmail.com>
2022-08-26 16:46:55 +0530

    Add meals details viewModel

---
+++ app/src/main/java/com/sample/mealzapp/ui/viewmodel/MealDetailsViewModel.kt
@@ -0,0 +1,26 @@
+package com.sample.mealzapp.ui.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.sample.mealzapp.data.models.Category
+import com.sample.mealzapp.data.repository.MealsRepository
+import com.sample.mealzapp.ui.navigations.ARG_MEAL
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MealDetailsViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    mealsRepository: MealsRepository
+) : ViewModel() {
+
+    private var _mealState = mutableStateOf<Category?>(null)
+    val mealState get() = _mealState.value
+
+    init {
+        val mealId = savedStateHandle.get<String>(ARG_MEAL)
+        _mealState.value = mealsRepository.getMeal(mealId)
+    }
+
+}
