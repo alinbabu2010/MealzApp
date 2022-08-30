4cdc3e3326d519c8c2cd4dd74cb86425a87b469f
Alin Babu <alinbabu2010@gmail.com>
2022-08-30 10:52:39 +0530

    Add animation for animating multiple components

--- app/src/main/java/com/sample/mealzapp/ui/screens/MealDetailsScreen.kt
+++ app/src/main/java/com/sample/mealzapp/ui/screens/MealDetailsScreen.kt
@@ -1,6 +1,9 @@
 package com.sample.mealzapp.ui.screens
 
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
@@ -10,8 +13,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.sample.mealzapp.R
 import com.sample.mealzapp.data.models.Category
@@ -19,15 +24,24 @@ import com.sample.mealzapp.data.models.Category
 @Composable
 fun MealDetailsScreen(category: Category) {
 
-    var isExpanded by remember {
-        mutableStateOf(false)
+    var profilePictureState by remember {
+        mutableStateOf(ProfilePictureState.Normal)
     }
 
-    val imageSize: Dp by animateDpAsState(
-        targetValue = if (isExpanded)
-            dimensionResource(R.dimen.meal_image_size_expanded)
-        else dimensionResource(R.dimen.meal_image_size_collapsed)
-    )
+    val transition = updateTransition(targetState = profilePictureState, label = "")
+
+    val imageSize: Dp by transition.animateDp(label = "") {
+        it.size
+    }
+
+    val color by transition.animateColor(label = "") {
+        it.color
+    }
+
+    val widthSize: Dp by transition.animateDp(label = "") {
+        it.borderWidth
+    }
+
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -35,7 +49,14 @@ fun MealDetailsScreen(category: Category) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Card {
+            Card(
+                modifier = Modifier.padding(dimensionResource(R.dimen.details_card_padding)),
+                shape = CircleShape,
+                border = BorderStroke(
+                    width = widthSize,
+                    color = color
+                )
+            ) {
                 AsyncImage(
                     model = category.imageUrl,
                     contentDescription = "Meal image",
@@ -48,8 +69,16 @@ fun MealDetailsScreen(category: Category) {
             Text(text = category.name)
         }
 
-        Button(onClick = { isExpanded = !isExpanded }) {
+        Button(onClick = {
+            profilePictureState = if (profilePictureState == ProfilePictureState.Normal)
+                ProfilePictureState.Expanded else ProfilePictureState.Normal
+        }) {
             Text(text = "Change state of meal profile picture")
         }
     }
+}
+
+enum class ProfilePictureState(val color: Color, val size: Dp, val borderWidth: Dp) {
+    Normal(Color.Magenta, 120.dp, 8.dp),
+    Expanded(Color.Green, 200.dp, 24.dp)
 }
