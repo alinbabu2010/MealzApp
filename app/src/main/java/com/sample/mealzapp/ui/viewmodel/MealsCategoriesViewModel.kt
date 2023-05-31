6fcbf2311a1446ef8b93f560d70a7dc5d668c131
Alin Babu <alinbabu2010@gmail.com>
2023-05-31 13:00:28 +0530

    Refactor `getMealsCategories` method based on UI state change.
    Also use state flow update method.

--- app/src/main/java/com/sample/mealzapp/ui/viewmodel/MealsCategoriesViewModel.kt
+++ app/src/main/java/com/sample/mealzapp/ui/viewmodel/MealsCategoriesViewModel.kt
@@ -2,6 +2,7 @@ package com.sample.mealzapp.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.sample.mealzapp.data.models.CategoryUiModel
 import com.sample.mealzapp.data.repository.MealsRepository
 import com.sample.mealzapp.data.wrappers.ResponseWrapper
 import com.sample.mealzapp.ui.states.MealsUiState
@@ -9,6 +10,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -26,24 +28,26 @@ class MealsCategoriesViewModel @Inject constructor(
 
     private fun getMealsCategories() {
 
-        _uiState.value = _uiState.value.copy(state = MealsUiState.State.Loading)
+        _uiState.update { it.copy(state = MealsUiState.State.Loading) }
 
         viewModelScope.launch(Dispatchers.IO) {
             when (val response = mealsRepository.getMealsCategories()) {
                 is ResponseWrapper.Success -> {
-                    _uiState.value = if (response.data.isNullOrEmpty()) {
-                        _uiState.value.copy(state = MealsUiState.State.Empty)
-                    } else {
-                        _uiState.value.copy(
-                            state = MealsUiState.State.Success,
-                            data = response.data
-                        )
+                    _uiState.update {
+                        if (response.data.isNullOrEmpty()) {
+                            it.copy(state = MealsUiState.State.Empty)
+                        } else {
+                            it.copy(
+                                state = MealsUiState.State.Success,
+                                data = response.data
+                            )
+                        }
                     }
-
                 }
-                is ResponseWrapper.Error -> _uiState.value = _uiState.value.copy(
-                    state = MealsUiState.State.Error
-                )
+
+                is ResponseWrapper.Error -> _uiState.update {
+                    it.copy(state = MealsUiState.State.Error)
+                }
             }
         }
 
